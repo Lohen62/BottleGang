463cd5798e1e719c4a1c05e12cf919148a6c7111
M3lani03 <M3lani03>
2019-08-20 14:08:41 +0200

    ajout du dossier centre + début intégration

---
+++ inc/centre/centre.php
@@ -0,0 +1,27 @@
+
+<div class=centre>
+    
+    <div class=nouveautés>
+        <div class=news>
+            <div class=carre></div>
+            <div class=text>Nouveautés</div>
+        </div>
+        <div class=news1></div>
+        <div class=news2></div>
+        <div class=news3></div>
+    </div>
+
+    <div class=selection>
+        <div class=select>
+            <div class=carre></div>
+            <div class=text>Notre selection</div>
+        </div>
+        <div class=selec1></div>
+        <div class=select2></div>
+        <div class=select3></div>
+    </div>
+
+    <div class=trait></div>
+    <div class=vins>Voir tous nos vins</div>
+
+</div>
