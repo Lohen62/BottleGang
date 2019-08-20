29bf5457a75a6663e146c5d07f2d00ea69fff64f
gautgel <gelle.gauthier@gmail.com>
2019-08-20 13:10:55 +0200

    slider terminé

---
+++ inc/slider/slider.php
@@ -0,0 +1,10 @@
+<div class="main-carousel">
+        <?php 
+                $img = glob('img/slider/*.{png,jpeg,jpg}', GLOB_BRACE);
+
+                foreach($img as $value){
+                ?>
+                <img class="carousel-cell" src="<?= $value ?>" alt="photo de léopard des neiges"/>
+
+        <?php } ?> 
+</div>
