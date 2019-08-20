6622af87f917aac4ba006176a08efcc2061e30d7
Lohen62 <bry.carbonnier@gmail.com>
2019-08-20 13:15:04 +0200

    footer en cours

--- inc/footer/footer.php
+++ inc/footer/footer.php
@@ -1,11 +1,13 @@
 
-<div class="footer"><h1>pomme</h1></div>
-
-
-
-
-
-
+<div class="footer">
+    <div id="presentation">
+        <p class="texteFooter">Livraison Soignée <br> 4 à 8 jours</p>
+        <p class="texteFooter">Conseils personnalisés <br> +333 554 65 00 17</p>
+        <p class="texteFooter">Paiement sécurisé</p>
+        <p class="texteFooter">Origine des produits <br>garantie</p>
+    </div>
+    <div class="ligneintermediaire"></div>
+</div>
 
 <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js%22%3E"></script>
 <script src="js/script.js"></script>
--- inc/header/header.php
+++ inc/header/header.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/slider.css">
+
     <title>BottleGang Accueil</title>
 </head>
 
 
 <body>
-
-<h1>bonsoir à tous ! vérification en cours !</h1>
