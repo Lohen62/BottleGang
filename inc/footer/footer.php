d039e99c02e214bab4cec17bd2ecbeb17951416e
Lohen62 <bry.carbonnier@gmail.com>
2019-08-20 11:25:38 +0200

    first commit

---
+++ inc/footer/footer.php
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+
+
+<?php include "inc/header/header.php"?>
+
+<?php include "inc/slider/slider.php"?>
+
+
+<?php include "inc//footer/footer.php"?>
+
+
