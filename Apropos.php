fd7aa332d987fb11058159a817ed8a0c5c011aa5
M3lani03 <M3lani03>
2019-08-21 15:42:27 +0200

    Apropos fini

---
+++ Apropos.php
@@ -0,0 +1,49 @@
+<?php include "inc/header/header.php"?>
+
+<div class="tout">
+
+    <div class="personne">
+
+        <div class="photo"></div>
+
+        <div class="bio">
+            <h2>Biographie</h2>
+            <p>Jean-Michel Bottlegang né 1918 à Ars-en-Ré
+            en Charente-Maritime. Mort en 1984 dans sa ville natale. 
+            Son père tient une vigne qu'il à hérité de son père,sa 
+            mère est mère au foyer afin d'élevée ces 6 enfants. 
+            Jean-Michel fait des études pour devenir sommelier à son 
+            20eme anniversaire, il a pour projet d'agrandir son réseau 
+            et de créer son entreprise avec l'aide de son frère cadet 
+            Georges.</p>
+        </div>
+
+    </div>
+
+    <div class="histoire">
+        <div class="histtext">
+            <h2>Histoire</h2>
+            <p>L'histoire de BOTTLEGANG commence avec 
+            Jean-Michel BOTTLEGANG, qui d'ébute sa carrière dans le 
+            vin à 21ans. Jean-Michel crée son entreprise en 1939, 
+            malgrés la guerre, il croit en la consommation du vin dans 
+            les cafés. Il s'organise avec son frère Georges,parti en 
+            Algérie, pour élaborer un vin rouge velouté, d'une qualité 
+            constante, qui fait la réputation de la maison.
+            Le marché est alors en plein essor et Jean-michel Bottlegang 
+            veut participer à son développement en aportant ce service 
+            indispensable.Il est l'un des premiers à proposer ce nouveau 
+            conditionnement.
+            Au fur est à mesure du temps, il se perfectionne dans différents 
+            vins (blanc,champagne,..).
+            Quatre-vingt années d'histoire et un esprit familial toujours 
+            aussi fort puisque c'est aujourd'hui la troisième génération 
+            qui est aux commandes.</p>
+        </div>
+    </div>
+
+</div>
+
+<div class="maps"></div>
+
+<?php include "inc//footer/footer.php"?>
