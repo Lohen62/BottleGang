1b1124a5236982bff78f635d5aae05ae669dac97
M3lani03 <M3lani03>
2019-08-21 16:38:59 +0200

    modif Apropos

--- Apropos.php
+++ Apropos.php
@@ -7,12 +7,14 @@
         <div class="photo"></div>
 
         <div class="bio">
-            <h2>Biographie</h2>
+            <h2 class="titre">Biographie</h2>
             <p>Jean-Michel Bottlegang né 1918 à Ars-en-Ré
-            en Charente-Maritime. Mort en 1984 dans sa ville natale. 
-            Son père tient une vigne qu'il à hérité de son père,sa 
-            mère est mère au foyer afin d'élevée ces 6 enfants. 
-            Jean-Michel fait des études pour devenir sommelier à son 
+            en Charente-Maritime.</br>Mort en 1984 dans sa ville natale.</p> 
+            </br>
+            <p>Son père tient une vigne qu'il à hérité de son père,</br>sa 
+            mère est mère au foyer afin d'élevée ces 6 enfants.</p> 
+            </br>
+            <p>Jean-Michel fait des études pour devenir sommelier à son 
             20eme anniversaire, il a pour projet d'agrandir son réseau 
             et de créer son entreprise avec l'aide de son frère cadet 
             Georges.</p>
@@ -22,18 +24,18 @@
 
     <div class="histoire">
         <div class="histtext">
-            <h2>Histoire</h2>
+            <h2 class="titre">Histoire</h2>
             <p>L'histoire de BOTTLEGANG commence avec 
             Jean-Michel BOTTLEGANG, qui d'ébute sa carrière dans le 
             vin à 21ans. Jean-Michel crée son entreprise en 1939, 
             malgrés la guerre, il croit en la consommation du vin dans 
             les cafés. Il s'organise avec son frère Georges,parti en 
             Algérie, pour élaborer un vin rouge velouté, d'une qualité 
-            constante, qui fait la réputation de la maison.
-            Le marché est alors en plein essor et Jean-michel Bottlegang 
+            constante, qui fait la réputation de la maison.</p></br>
+            <p>Le marché est alors en plein essor et Jean-michel Bottlegang 
             veut participer à son développement en aportant ce service 
             indispensable.Il est l'un des premiers à proposer ce nouveau 
-            conditionnement.
+            conditionnement.</br>
             Au fur est à mesure du temps, il se perfectionne dans différents 
             vins (blanc,champagne,..).
             Quatre-vingt années d'histoire et un esprit familial toujours 
