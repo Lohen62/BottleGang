e9f3008141d765abd22e65b658ca3f7ea5d0b53d
Lohen62 <bry.carbonnier@gmail.com>
2019-08-22 15:33:56 +0200

    sa avance, mais j'suis pas backiste

--- panier.php
+++ panier.php
@@ -1,25 +1,69 @@
 <?php
 require("inc/dbconnect.php");
 session_start();
-$panier = 6;
+
+
+$panier = [1,2,3,4];
+$panier = implode(",",$panier);
+
+var_dump($panier);
+
+
 include("inc/header/header.php");
 
-$sql = "SELECT * FROM vins WHERE id = '" . $panier ."' ";
+
+
+// $sql = "SELECT * FROM vins WHERE id = '" . $panier ."' ";
+
+$sql = "SELECT * FROM vins WHERE id IN ($panier)";
+  $data = $bdd->query($sql);
+
+  $data->setFetchMode(PDO::FETCH_OBJ);
 
     echo "<br> <br> <br> <br> <br>";
 
-    $data = $bdd->query($sql);
+        function DeleteId(){
+            array_splice($panier,$rows->id,1);
+        }
 
-    $data->setFetchMode(PDO::FETCH_OBJ);
-
-    while($rows = $data->fetch()){
-        echo 'id : '. $rows->id . " / name : " . $rows->nom . "<br>";
-}
+   
 ?>
+    <table>
+        <thead>
+            <tr>
+            <td></td>
+            <td>Nom</td>
+            <td>Annee</td>
+            <td>Description</td>
+            <td>Prix</td>
+            <td>Effacer</td>
+            </tr>
+        </thead>
+        <tbody>
+
+     <?php 
+
+        while($rows = $data->fetch()){
+        echo " 
+                <tr>
+                    <td></td>
+                    <td> " .$rows->nom . "  </td>
+                    <td>" .$rows->annee . " </td>
+                    <td>" .$rows->description . " </td>
+                    <td>" .$rows->prix . " € </td>
+                    <td> <button onclick='DeleteId()'>Supprimez-moi!</button> </td>
+               </tr>
+                ";
+            }
+    ?>
+        </tbody>
+    </table>
+
 
 </header>
 
-
+<!-- while($rows = $data->fetch()){
+        echo 'id : '. $rows->id . " / name : " . $rows->nom . "<br>"; -->
 
 <?php
 include("inc/footer/footer.php");
