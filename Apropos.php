39375e795b71ad72e823b8b7c0af8f46614c81ae
gautgel <gelle.gauthier@gmail.com>
2019-08-23 22:00:32 +0200

    apropos modifié animation texte

--- Apropos.php
+++ Apropos.php
@@ -1,8 +1,35 @@
 <?php  include "inc/header/header.php";?>
 
 <div class="tout">
-    <span class="text1">Bottle Gang</span>
-    <span class="text2">Test bla bla bla</span>
+    <div class="cache"></div>
+    <span class="titreBG anim-writing">Bottle Gang</span>
+    <span class="texteJM">Jean-Michel Bottlegang né 1918 à Ars-en-Ré
+            en Charente-Maritime.Mort en 1984 dans sa ville natale.</p> 
+            
+            Son père tient une vigne qu'il à hérité de son père,sa 
+            mère est mère au foyer afin d'élevée ces 6 enfants.</p> 
+            
+            Jean-Michel fait des études pour devenir sommelier à son 
+            20eme anniversaire, il a pour projet d'agrandir son réseau 
+            et de créer son entreprise avec l'aide de son frère cadet 
+            Georges.<br><br>
+            L'histoire de BOTTLEGANG commence avec 
+            Jean-Michel BOTTLEGANG, qui d'ébute sa carrière dans le 
+            vin à 21ans.<br><br> Jean-Michel crée son entreprise en 1939, 
+            malgrés la guerre, il croit en la consommation du vin dans 
+            les cafés. Il s'organise avec son frère Georges,parti en 
+            Algérie, pour élaborer un vin rouge velouté, d'une qualité 
+            constante, qui fait la réputation de la maison.
+            Le marché est alors en plein essor et Jean-michel Bottlegang 
+            veut participer à son développement en aportant ce service 
+            indispensable. Il est l'un des premiers à proposer ce nouveau 
+            conditionnement.<br><br>
+            Au fur est à mesure du temps, il se perfectionne dans différents 
+            vins (blanc,champagne,..).
+            Quatre-vingt années d'histoire et un esprit familial toujours 
+            aussi fort puisque c'est aujourd'hui la troisième génération 
+            qui est aux commandes.
+    </span>
     <!-- <div class="personne">
 
         <div class="photo"></div>
@@ -10,7 +37,7 @@
         <div class="bio">
             <h2 class="titre">Biographie</h2>
             <p>Jean-Michel Bottlegang né 1918 à Ars-en-Ré
-            en Charente-Maritime.</br>Mort en 1984 dans sa ville natale.</p> 
+            en Charente-Maritime.Mort en 1984 dans sa ville natale.</p> 
             </br>
             <p>Son père tient une vigne qu'il à hérité de son père,</br>sa 
             mère est mère au foyer afin d'élevée ces 6 enfants.</p> 
