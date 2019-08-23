c118a8fb863a0ab60c71bc375a8cbf61b7d32083
gautgel <gelle.gauthier@gmail.com>
2019-08-23 22:03:39 +0200

    texte modifie

--- Apropos.php
+++ Apropos.php
@@ -4,9 +4,9 @@
     <div class="cache"></div>
     <span class="titreBG anim-writing">Bottle Gang</span>
     <span class="texteJM">Jean-Michel Bottlegang né 1918 à Ars-en-Ré
-            en Charente-Maritime.Mort en 1984 dans sa ville natale.</p> 
+            en Charente-Maritime. Mort en 1984 dans sa ville natale.</p> 
             
-            Son père tient une vigne qu'il à hérité de son père,sa 
+            Son père tient une vigne qu'il à hérité de son père, sa 
             mère est mère au foyer afin d'élevée ces 6 enfants.</p> 
             
             Jean-Michel fait des études pour devenir sommelier à son 
@@ -15,16 +15,16 @@
             Georges.<br><br>
             L'histoire de BOTTLEGANG commence avec 
             Jean-Michel BOTTLEGANG, qui d'ébute sa carrière dans le 
-            vin à 21ans.<br><br> Jean-Michel crée son entreprise en 1939, 
+            vin à 21 ans.<br><br> Jean-Michel crée son entreprise en 1939, 
             malgrés la guerre, il croit en la consommation du vin dans 
-            les cafés. Il s'organise avec son frère Georges,parti en 
+            les cafés. Il s'organise avec son frère Georges, parti en 
             Algérie, pour élaborer un vin rouge velouté, d'une qualité 
             constante, qui fait la réputation de la maison.
             Le marché est alors en plein essor et Jean-michel Bottlegang 
             veut participer à son développement en aportant ce service 
             indispensable. Il est l'un des premiers à proposer ce nouveau 
             conditionnement.<br><br>
-            Au fur est à mesure du temps, il se perfectionne dans différents 
+            Au fur et à mesure du temps, il se perfectionne dans différents 
             vins (blanc,champagne,..).
             Quatre-vingt années d'histoire et un esprit familial toujours 
             aussi fort puisque c'est aujourd'hui la troisième génération 
