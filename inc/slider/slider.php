5f141be7f8e7089138f65ca512a053dde4ec017a
RoxBibop <thibault.malik@gmail.com>
2019-08-24 11:58:47 +0200

    a propos justify

--- inc/slider/slider.php
+++ inc/slider/slider.php
@@ -12,7 +12,7 @@
 <div class="infos">
         <p class="pres">Depuis l'ouverture de leurs première cave en 1939, cette fabrique famillial de luxe, Bottle Gang , s'éfforce de produire et vendre, des vins riche, d'histoires, de qualitées, et de saveurs  uniques, afin de ravire vos palais.</p>
         <p class="pres it">"Une belle robe pour un vin de caractère".</p>
-        <a class="btn" href="">voir nos produits</a>
+        <a class="btn" href="./vins.php">voir nos produits</a>
 </div>
 
 </header>
